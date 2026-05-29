84c4e01d2196ede41d43b6862c07b27453d24440
Nazym Beisembin <nazym.beisembin@uni-a.de>
2026-05-29 13:08:45 +0200

    Revert "Merge branch 'beisemna_06' into 'main'"
    This reverts merge request !30

--- TutorialProject/Data/Lessons/StaticLessonRepository.cs
+++ TutorialProject/Data/Lessons/StaticLessonRepository.cs
@@ -1,62 +1,25 @@
+namespace TutorialProject.Data.Lessons;
 using TutorialProject.Models;
 
-namespace TutorialProject.Data.Lessons;
+public class StaticLessonRepository : ILessonRepository{
 
-public class StaticLessonRepository : ILessonRepository
+    private List<Lesson> lessons;
+
+
+
+public StaticLessonRepository()
 {
-    private readonly List<Lesson> lessons;
-
-    public StaticLessonRepository()
+    lessons = new List<Lesson>
     {
-        lessons = new List<Lesson>
-        {
-            new Lesson
-            {
-                Id = 1234,
-                Title = "Softwareprojekt",
-                CardDeckLink = "https://sopro.makandra.de",
-                TimeEstimation = 2.0
-            }
-        };
-    }
+        new Lesson {Id = 1234, Title = "Softwareptojekt", CardDeckLink = "sopro.makandra.de", TimeEstimation = 2.0}
+    };
+}
 
-    public IEnumerable<Lesson> GetAllLessons()
+public IEnumerable<Lesson> GetAllLessons()
     {
         return lessons;
     }
-
-    public bool Exists(int id)
-    {
-        return lessons.Any(lesson => lesson.Id == id);
-    }
-
-    public bool Exists(Lesson lesson)
-    {
-        return Exists(lesson.Id);
-    }
-
-    public void Create(Lesson lesson)
-    {
-        if (!Exists(lesson))
-        {
-            lessons.Add(lesson);
-        }
-    }
-
-    public void Update(Lesson lesson)
-    {
-        var existingLesson = lessons.FirstOrDefault(l => l.Id == lesson.Id);
-
-        if (existingLesson != null)
-        {
-            existingLesson.Title = lesson.Title;
-            existingLesson.CardDeckLink = lesson.CardDeckLink;
-            existingLesson.TimeEstimation = lesson.TimeEstimation;
-        }
-    }
-
-    public void Delete(Lesson lesson)
-    {
-        lessons.RemoveAll(l => l.Id == lesson.Id);
-    }
 }
+
+
+
