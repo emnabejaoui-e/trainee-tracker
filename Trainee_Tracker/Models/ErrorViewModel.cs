4f19a0ce1a014ea2070b7881a15068696f5bfc7c
julia <julia-egling@t-online.de>
2026-06-08 13:01:00 +0200

    feat: add Trainee_Tracker mvc project

---
+++ Trainee_Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Trainee_Tracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
