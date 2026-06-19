aeb39d440a95c0971d0a9263aeaba9728eecd3d0
unknown <jelena.cosic@uni-a.de>
2026-06-19 14:46:52 +0200

    Add IUserService and UserService

---
+++ Trainee_Tracker/Services/UserService.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using Trainee_Tracker.Models;
+using Trainee_Tracker.Repositories;
+
+namespace Trainee_Tracker.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void CreateTrainee(Trainee trainee, string rawPassword)
+        {
+            var hashedPassword = HashPassword(rawPassword);
+            _userRepository.CreateTrainee(trainee, hashedPassword);
+        }
+
+        public void CreateMentor(Mentor mentor, string rawPassword)
+        {
+            var hashedPassword = HashPassword(rawPassword);
+            _userRepository.CreateMentor(mentor, hashedPassword);
+        }
+
+        public void CreateAdmin(Admin admin, string rawPassword)
+        {
+            var hashedPassword = HashPassword(rawPassword);
+            _userRepository.CreateAdmin(admin, hashedPassword);
+        }
+
+        public void UpdateTrainee(Trainee trainee)
+        {
+            _userRepository.UpdateUser(trainee);
+        }
+
+        public void UpdateMentor(Mentor mentor)
+        {
+            _userRepository.UpdateUser(mentor);
+        }
+
+        public void UpdateAdmin(Admin admin)
+        {
+            _userRepository.UpdateUser(admin);
+        }
+
+        public IList<User> GetAllUsers()
+        {
+            return _userRepository.GetAllUsers();
+        }
+
+        public void CloseUser(string email)
+        {
+            _userRepository.CloseUser(email);
+        }
+
+        public LoginResult ValidateUserCredentials(string email, string password)
+        {
+            var user = _userRepository.GetUserByEmail(email);
+
+            if (user == null)
+            {
+                return LoginResult.InvalidCredentials;
+            }
+
+            if (user.Closed)
+            {
+                return LoginResult.AccountClosed;
+            }
+
+            var credentialsValid = _userRepository.ValidateUserCredentials(email, password);
+
+            return credentialsValid ? LoginResult.Success : LoginResult.InvalidCredentials;
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            return _userRepository.GetUserByEmail(email);
+        }
+
+        private static string HashPassword(string rawPassword)
+        {
+
+            return rawPassword;
+        }
+    }
+}
