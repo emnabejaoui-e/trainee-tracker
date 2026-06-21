755d795a99a2d3c54d8295c82b7fc923a1084fc2
julia <julia-egling@t-online.de>
2026-06-21 23:17:28 +0200

    feat: add interface ILessonAssignmentRepository to Data

---
+++ Trainee_Tracker/Data/LessonAssignments/ILessonAssignmentRepository.cs
@@ -0,0 +1,11 @@
+using Trainee_Tracker.Models;
+
+namespace Trainee_Tracker.Data.LessonAssignments;
+
+public interface ILessonAssignmentRepository
+{
+    //public List<LessonAssignment> FindByTrainee(Trainee trainee);
+    //public List<LessonAssignment> FindByStatus(Trainee trainee, LessonAssignmentStatus status);
+     public void Save(LessonAssignment assignment);
+     public void Delete(LessonAssignment assignment);
+}
