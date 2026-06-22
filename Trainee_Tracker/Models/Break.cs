dae8ca3a7f4f6591e30528c26cb43b56ce24a542
Andrej Basara <andrej.basara@uni-a.de>
2026-06-22 12:48:13 +0200

    Break Id und traine objekt

--- Trainee_Tracker/Models/Break.cs
+++ Trainee_Tracker/Models/Break.cs
@@ -7,8 +7,9 @@ namespace Trainee_Tracker.Models
 {
     public class Break
     {
+        public int Id {get; init; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
-        public int TraineeId { get; set; }
+        public Trainee Trainee { get; set; }
     }
 }
