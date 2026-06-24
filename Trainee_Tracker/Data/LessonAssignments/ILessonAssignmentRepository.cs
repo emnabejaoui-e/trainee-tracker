357cf461e510f4559ee41d9564664222d22cd321
julia <julia-egling@t-online.de>
2026-06-24 22:47:46 +0200

    feat: add method GetAllLessonAssignments in ILessonAssignmentRepository.cs

--- Trainee_Tracker/Data/LessonAssignments/ILessonAssignmentRepository.cs
+++ Trainee_Tracker/Data/LessonAssignments/ILessonAssignmentRepository.cs
@@ -6,6 +6,8 @@ public interface ILessonAssignmentRepository
 {
     public List<LessonAssignment> FindByTrainee(Trainee trainee);
     public List<LessonAssignment> FindByStatus(Trainee trainee, LessonAssignmentStatus status);
-     public void Save(LessonAssignment assignment);
-     public void Delete(LessonAssignment assignment);
+    public void Save(LessonAssignment assignment);
+    public void Delete(LessonAssignment assignment);
+    public IEnumerable<LessonAssignment> GetAllLessonAssignments();
+
 }
