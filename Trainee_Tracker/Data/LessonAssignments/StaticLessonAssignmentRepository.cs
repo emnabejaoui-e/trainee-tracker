e4c51b4c5a4854fab0f3d2ffee63fbf2546884ab
julia <julia-egling@t-online.de>
2026-06-25 15:09:48 +0200

    feat: add more dummy data

--- Trainee_Tracker/Data/LessonAssignments/StaticLessonAssignmentRepository.cs
+++ Trainee_Tracker/Data/LessonAssignments/StaticLessonAssignmentRepository.cs
@@ -15,11 +15,21 @@ public class StaticLessonAssignemtRepository : ILessonAssignmentRepository
             new LessonAssignment(3, new Lesson {Id = 33, Title = "Ruby: What extend and include do", Effort = 2.0, Inactive = false, Position = 4}, 4, new DateOnly(2026, 6, 26)),
             new LessonAssignment(4, new Lesson {Id = 44, Title = "Rules of thumb against flaky specs", URL ="#", Effort = 0.5, Inactive = false, Position = 5}, 5, new DateOnly(2026, 6, 26)),
             new LessonAssignment(5, new Lesson {Id = 55, Title = "How to use API", URL ="#", Effort = 1.5, Inactive = false, Position = 6}, 6, new DateOnly(2026, 6, 27)),
+            new LessonAssignment(6, new Lesson {Id = 66, Title = "Linux", URL ="#", Effort = 2.0, Inactive = false, Position = 7}, 7, new DateOnly(2026, 6, 28)),
+            new LessonAssignment(7, new Lesson {Id = 77, Title = "Linux file system", URL ="#", Effort = 1.0, Inactive = false, Position = 7}, 7, new DateOnly(2026, 6, 30)),
+            new LessonAssignment(8, new Lesson {Id = 88, Title = "Resource use", URL ="#", Effort = 1.0, Inactive = false, Position = 8}, 8, new DateOnly(2026, 6, 30)),
+            new LessonAssignment(9, new Lesson {Id = 99, Title = "Linux Kernal parameter", URL ="#", Effort = 0.5, Inactive = false, Position = 9}, 9, new DateOnly(2026, 7, 01)),
+            new LessonAssignment(10, new Lesson {Id = 100, Title = "Network", URL ="#", Effort = 4.0, Inactive = false, Position = 10}, 10, new DateOnly(2026, 7, 01)),
         };
 
-        _data[4].StartAssignment();
-        _data[3].FinishAssignment();
-        _data[2].FinishAssignment();
+        _data[4].RateAssignemnt();
+        _data[3].AcceptAssignment();
+        _data[2].AcceptAssignment();
+        _data[5].RateAssignemnt();
+        _data[6].FinishAssignment();
+        _data[7].FinishAssignment();
+        _data[8].StartAssignment();
+        
 
     }
 
