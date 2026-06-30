1cdd01c8d12949ee3ae277a2ed3f75795b4e7e68
Leon <leon.paintner@uni-a.de>
2026-06-30 21:38:16 +0200

    refactor: Move Curriculum import into Curriculum class

--- Trainee_Tracker/Models/Curriculum.cs
+++ Trainee_Tracker/Models/Curriculum.cs
@@ -6,12 +6,18 @@ namespace Trainee_Tracker.Models;
 /// <summary>
 /// A named collection of Lessons.
 /// </summary>
+/// <author>Leon</author>
 public class Curriculum
 {
     public string Title { get; set; }
     public IList<Lesson> Lessons { get; set; }
 
     public void AddLesson(Lesson lesson)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(IList<Lesson> Lessons)
     {
         throw new NotImplementedException();
     }
--- Trainee_Tracker/Services/IMentorService.cs
+++ Trainee_Tracker/Services/IMentorService.cs
@@ -12,18 +12,4 @@ public interface IMentorService
     /// <param name="traineeId">The numeric id of the Trainee to assign.</param>
     /// <returns></returns>
     void AssignTraineeToMentor(int mentorId, int traineeId);
-    
-    /// <summary>
-    /// Changes the order of lessons for a particular trainee.
-    /// </summary>
-    /// <param name="traineeId">The numeric id of the Trainee to change the order for.</param>
-    /// <param name="order">A list with the numeric ids of Lessons in the order they should appear for trainees.</param>
-    void UpdateLessonOrder(int traineeId, IList<int> order);
-
-    /// <summary>
-    /// Imports 
-    /// </summary>
-    /// <param name="curriculumName"></param>
-    /// <param name="curriculum"></param>
-    void ImportCurriculum(string curriculumName, IList<Lesson> curriculum);
 }
