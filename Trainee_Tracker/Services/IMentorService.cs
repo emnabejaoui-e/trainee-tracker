f0f860358119caa690868c0529917d86fb8d265e
Leon <leon.paintner@uni-a.de>
2026-06-30 20:56:25 +0200

    feat: Add UpdateLessonOrder to Mentor Service

--- Trainee_Tracker/Services/IMentorService.cs
+++ Trainee_Tracker/Services/IMentorService.cs
@@ -4,9 +4,17 @@ namespace Trainee_Tracker.Services;
 public interface IMentorService
 {
     /// <summary>
+    /// Assigns a Mentor to a trainee.
+    /// </summary>
+    /// <param name="mentorId">The numeric id of the Mentor to assign.</param>
+    /// <param name="traineeId">The numeric id of the Trainee to assign.</param>
+    /// <returns></returns>
+    void AssignTraineeToMentor(int mentorId, int traineeId);
+    
+    /// <summary>
     /// Changes the order of lessons for a particular trainee.
     /// </summary>
     /// <param name="traineeId">The numeric id of the Trainee to change the order for.</param>
     /// <param name="order">A list with the numeric ids of Lessons in the order they should appear for trainees.</param>
-    void AssignTraineeToMentor(int mentorId, int traineeId);
+    void UpdateLessonOrder(int traineeId, IList<int> order);
 }
