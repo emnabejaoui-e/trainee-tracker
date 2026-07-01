c7a55c73130647fcc17f158abf266934ab80a61d
julia <julia-egling@t-online.de>
2026-07-01 11:20:34 +0200

    info: ready to merge

--- Trainee_Tracker/Data/AppDbContext.cs
+++ Trainee_Tracker/Data/AppDbContext.cs
@@ -13,8 +13,7 @@ public class AppDbContext : DbContext
     public DbSet<Mentor> Mentors { get; set; }
     public DbSet<Admin> Admins { get; set; }
     
-    public DbSet<Lesson> Lessons{get; set;} //vor dem Mergen rausnehmen weil in branch Lessonassignment-db schon hinzugefügt
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasDiscriminator<string>("Role")
