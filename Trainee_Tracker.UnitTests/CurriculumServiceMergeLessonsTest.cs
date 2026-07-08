b12365ea9e0f26ce2ad77cd247afdfef19b3a5e3
Leon Paintner <leon.paintner@uni-a.de>
2026-07-08 20:17:36 +0200

    feat: Add test that removed lessons are marked as inactive

--- Trainee_Tracker.UnitTests/CurriculumServiceMergeLessonsTest.cs
+++ Trainee_Tracker.UnitTests/CurriculumServiceMergeLessonsTest.cs
@@ -20,4 +20,33 @@ public class CurriculumServiceMergeLessonsTest
         
         Assert.Empty(result);
     }
+    
+    [Fact]
+    public void TestRemovedLessonsAreMarkedAsInactive()
+    {
+        var existingLessons = new List<Lesson>()
+        {
+            new()
+            {
+                Id = 1,
+            },
+            new()
+            {
+                Id = 2,
+            }
+        };
+        var importedLessons = new List<Lesson>()
+        {
+            new()
+            {
+                Id = 2,
+            }
+        };
+
+        var result = _service.MergeLessons(existingLessons, importedLessons);
+
+        var removedLesson = result.First(l => l.Id == 1);
+        
+        Assert.True(removedLesson.Inactive, "Removed Lesson should be inactive.");
+    }
 }
