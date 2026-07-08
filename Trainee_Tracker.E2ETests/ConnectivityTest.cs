f696ef78cf1cca0b2aeb050f9b01565dc697ce79
Leon <leon.paintner@uni-a.de>
2026-07-08 20:47:30 +0200

    feat(test): Add small sanity check that ensures that the project is reachable

---
+++ Trainee_Tracker.E2ETests/ConnectivityTest.cs
@@ -0,0 +1,15 @@
+namespace Trainee_Tracker.E2ETests;
+
+public class ConnectivityTest
+{
+    private readonly HttpClient _client = new ();
+    private const string BaseUrl = "http://localhost:5089";
+
+    [Fact]
+    public async void ServerReturnsSuccessStatus()
+    {
+        var response = await _client.GetAsync(BaseUrl);
+
+        response.EnsureSuccessStatusCode();
+    }
+}
