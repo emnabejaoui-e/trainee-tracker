4ce6e87d7a58de4bb9207ff9f29c202d76075274
Andrej Basara <andrej.basara@uni-a.de>
2026-07-10 14:18:45 +0200

    fix controller conflicts with authorization

--- Trainee_Tracker/Controllers/Profile.cs
+++ Trainee_Tracker/Controllers/Profile.cs
@@ -1,21 +1,44 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Trainee_Tracker.Data.MentorRepository;
 using Trainee_Tracker.Data.TraineeRepository;
 
 namespace Trainee_Tracker.Controllers
 {
     [Route("[controller]")]
-    [Authorize(Roles = "Trainee")]
     public class Profile : Controller
     {
         private readonly ITraineeRepository _traineeRepo;
+        private readonly IMentorRepository _mentorRepo;
 
-        public Profile(ITraineeRepository traineeRepo)
+        public Profile(ITraineeRepository traineeRepo, IMentorRepository mentorRepo)
         {
             _traineeRepo = traineeRepo;
+            _mentorRepo = mentorRepo;
         }
 
+        [HttpGet("MentorProfile")]
+        [Authorize(Roles = "Mentor, Admin")]
+        public IActionResult MentorProfile()
+        {
+            var mentorIdstring = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (mentorIdstring == null || !int.TryParse(mentorIdstring, out var mentorId))
+            {
+                return Unauthorized();
+            }
+
+            var mentor = _mentorRepo.GetMentorById(mentorId);
+            if (mentor == null)
+            {
+                return Unauthorized();
+            }
+
+            return View("MyProfileMentor", mentor);
+        }
+
+        [HttpGet("TraineeProfile")]
+        [Authorize(Roles = "Trainee")]
         public IActionResult TraineeProfile()
         {
             var traineeIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
