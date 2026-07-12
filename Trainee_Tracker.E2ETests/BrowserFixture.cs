5fad122e6d6f65bc18d954429d229189b0006af1
Andrej Basara <andrej.basara@uni-a.de>
2026-07-12 15:34:20 +0200

    BroweserFixture for global use

---
+++ Trainee_Tracker.E2ETests/BrowserFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tutorial_Project.E2ETests;
+
+public class BrowserFixture : IDisposable
+{
+    public IWebDriver driver;
+    public BrowserFixture()
+    {
+        var chromeOptions = new ChromeOptions();
+        chromeOptions.AddArgument("--headless");
+        chromeOptions.AddArgument("--window-size=1920,1080");
+        chromeOptions.AddArgument("--no-sandbox");
+        chromeOptions.AddArgument("--disable-dev-shm-usage");
+        driver = new ChromeDriver(chromeOptions);
+
+        
+    }
+
+    public void Dispose()
+    {
+        driver?.Quit();
+
+    }
+}
