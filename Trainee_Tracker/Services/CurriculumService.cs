19f795626f12fc89e54919861155e520f3ba96a9
Leon Paintner <leon.paintner@uni-a.de>
2026-07-12 19:38:35 +0200

    feat: Add logic to create and modify Lesson Assignments for Lesson import

--- Trainee_Tracker/Services/CurriculumService.cs
+++ Trainee_Tracker/Services/CurriculumService.cs
@@ -1,6 +1,7 @@
 // Code Owner: Leon Paintner
 
 using Trainee_Tracker.Data.Curriculums;
+using Trainee_Tracker.Data.LessonAssignments;
 using Trainee_Tracker.Data.Lessons;
 using Trainee_Tracker.Data.TraineeRepository;
 using Trainee_Tracker.Models;
@@ -12,12 +13,14 @@ public class CurriculumService : ICurriculumService
     private readonly ILessonRepository _lessonRepo;
     private readonly ICurriculumRepository _curriculumRepo;
     private readonly ITraineeRepository _traineeRepo;
+    private readonly ILessonAssignmentRepository _lessonAssignmentRepo;
 
-    public CurriculumService(ILessonRepository lessonRepo, ICurriculumRepository curriculumRepo, ITraineeRepository traineeRepo)
+    public CurriculumService(ILessonRepository lessonRepo, ICurriculumRepository curriculumRepo, ITraineeRepository traineeRepo, ILessonAssignmentRepository lessonAssignmentRepo)
     {
         _lessonRepo = lessonRepo;
         _curriculumRepo = curriculumRepo;
         _traineeRepo = traineeRepo;
+        _lessonAssignmentRepo = lessonAssignmentRepo;
     }
 
     public IList<Lesson> MergeLessons(IList<Lesson> existingLessons, IList<Lesson> importedLessons)
@@ -68,6 +71,8 @@ public class CurriculumService : ICurriculumService
 
     public void Update(Curriculum updatedCurriculum)
     {
+        var trainees = GetTraineesOfCurriculum(updatedCurriculum);
+        
         for (int i = 0; i < updatedCurriculum.Lessons.Count; i++)
         {
             var lesson = updatedCurriculum.Lessons[i];
@@ -75,11 +80,40 @@ public class CurriculumService : ICurriculumService
             lesson.Position = i;
             
             _lessonRepo.Update(lesson);
+            
+            var assignments = _lessonAssignmentRepo.FindByLesson(lesson);
+            foreach (var lessonAssignment in assignments)
+            {
+                if (lesson.Inactive && lessonAssignment.Status == LessonAssignmentStatus.Open)
+                {
+                    lessonAssignment.SkipAssignment();
+                    _lessonAssignmentRepo.Save(lessonAssignment);
+                }
+            }
+
+            var traineesWithMissingAssignments = trainees.Where(t => !assignments.Any(la => la.TraineeId == t.Id));
+            foreach (var trainee in traineesWithMissingAssignments)
+            {
+                var assignmentsOfTrainee = _lessonAssignmentRepo.FindByTrainee(trainee)
+                    .Select(la => la.Id)
+                    .ToList();
+                
+                var assignment = new LessonAssignment
+                {
+                    Lesson = lesson,
+                    LessonId = lesson.Id,
+                    Position = lesson.Position,
+                    Trainee = trainee,
+                    TraineeId = trainee.Id
+                };
+                _lessonAssignmentRepo.Save(assignment);
+                
+                assignmentsOfTrainee.Insert(Math.Min(lesson.Position, assignmentsOfTrainee.Count - 1), assignment.Id);
+                _lessonAssignmentRepo.UpdateAssignmentPositions(assignmentsOfTrainee);
+            }
         }
         
         _curriculumRepo.Update(updatedCurriculum);
-        
-        // TODO: Create/update Lesson assignments for trainees.
     }
 
     public ICollection<Trainee> GetTraineesOfCurriculum(Curriculum curriculum)
