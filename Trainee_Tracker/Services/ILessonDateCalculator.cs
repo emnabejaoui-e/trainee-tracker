642a50fc4202414ffaebfce193cd66dc954a7ebd
Andrej Basara <andrej.basara@uni-a.de>
2026-07-17 16:28:48 +0200

    weekplan refactor

--- Trainee_Tracker/Services/ILessonDateCalculator.cs
+++ Trainee_Tracker/Services/ILessonDateCalculator.cs
@@ -5,4 +5,5 @@ namespace Trainee_Tracker.Services;
 public interface ILessonDateCalculator
 {
     List<LessonAssignment> RecalculateRoughExpectedDates(Trainee trainee);
+    List<LessonAssignment> GetAssignmentsForCurrentWeek(Trainee trainee, out DateOnly weekStart);
 }
