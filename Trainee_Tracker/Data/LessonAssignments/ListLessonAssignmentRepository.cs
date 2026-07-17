7e46d506cdccecd19159c6d9f0c15c67c1ef7721
Leon Paintner <leon.paintner@uni-a.de>
2026-07-17 13:00:54 +0200

    feat(test): Add dummy ILessonAssignmentRepository

---
+++ Trainee_Tracker/Data/LessonAssignments/ListLessonAssignmentRepository.cs
@@ -0,0 +1,71 @@
+using Trainee_Tracker.Models;
+
+namespace Trainee_Tracker.Data.LessonAssignments;
+
+public class ListLessonAssignmentRepository : ILessonAssignmentRepository
+{
+    private readonly IList<LessonAssignment> _data;
+
+    public ListLessonAssignmentRepository(IList<LessonAssignment> data)
+    {
+        _data = data;
+    }
+
+    public List<LessonAssignment> FindByTrainee(Trainee trainee)
+    {
+        return _data.Where(la => la.TraineeId == trainee.Id)
+            .ToList();
+    }
+
+    public List<LessonAssignment> FindByStatus(Trainee trainee, LessonAssignmentStatus status)
+    {
+        return _data.Where(la => la.TraineeId == trainee.Id)
+            .Where(la => la.Status == status)
+            .ToList();
+    }
+
+    public List<LessonAssignment> FindByLesson(Lesson lesson)
+    {
+        return _data.Where(la => la.LessonId == lesson.Id)
+            .ToList();
+    }
+
+    public void Save(LessonAssignment assignment)
+    {
+        _data.Remove(assignment);
+        _data.Add(assignment);
+    }
+
+    public void Delete(LessonAssignment assignment)
+    {
+        _data.Remove(assignment);
+    }
+
+    public IEnumerable<LessonAssignment> GetAllLessonAssignments()
+    {
+        return _data.AsReadOnly();
+    }
+
+    public void UpdateStatus(int id, LessonAssignmentStatus newStatus)
+    {
+        _data.First(la => la.Id == id).Status = newStatus;
+    }
+
+    public void UpdatePosition(int id, int newPosition)
+    {
+        _data.First(la => la.Id == id).Position = newPosition;
+    }
+
+    public void UpdateAssignmentPositions(List<int> orderedAssignmentIds)
+    {
+        for (var i = 0; i < orderedAssignmentIds.Count; i++)
+        {
+            _data.First(la => la.Id == orderedAssignmentIds[i]).Position = i + 1;
+        }
+    }
+
+    public LessonAssignment? GetById(int assignmentId)
+    {
+        return _data.FirstOrDefault(la => la.Id == assignmentId, null);
+    }
+}
