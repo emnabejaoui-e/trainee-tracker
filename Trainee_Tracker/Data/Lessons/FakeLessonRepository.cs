fcc79085ae01d1aab895393fa16f31aa168cab11
Leon <leon.paintner@uni-a.de>
2026-07-18 11:24:36 +0200

    feat(test): Implement fake repositories for Unit testing

---
+++ Trainee_Tracker/Data/Lessons/FakeLessonRepository.cs
@@ -0,0 +1,34 @@
+using Trainee_Tracker.Models;
+
+namespace Trainee_Tracker.Data.Lessons;
+
+public class FakeLessonRepository : ILessonRepository
+{
+    private readonly IList<Lesson> _data = new List<Lesson>();
+    
+    public Lesson? FindById(int id)
+    {
+        return _data.FirstOrDefault(l => l.Id == id);
+    }
+
+    public IEnumerable<Lesson> GetAllLessons()
+    {
+        return _data.AsReadOnly();
+    }
+
+    public void Delete(Lesson lesson)
+    {
+        _data.Remove(lesson);
+    }
+
+    public void Update(Lesson lesson)
+    {
+        _data.Remove(lesson);
+        _data.Add(lesson);
+    }
+
+    public void Add(Lesson lesson)
+    {
+        _data.Add(lesson);
+    }
+}
