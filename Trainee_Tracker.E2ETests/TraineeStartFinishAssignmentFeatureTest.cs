eb1145e87722d019873d976c6ec6c2b4a36a1e9a
julia <julia-egling@t-online.de>
2026-07-25 16:33:42 +0200

    fix: fix e2etest for pipeline

--- Trainee_Tracker.E2ETests/TraineeStartFinishAssignmentFeatureTest.cs
+++ Trainee_Tracker.E2ETests/TraineeStartFinishAssignmentFeatureTest.cs
@@ -33,9 +33,10 @@ public class TraineeStartFinishAssignmentFeatureTest
 
         //Arragne: assignment start
         var openColumn = _wait.Until(d => d.FindElement(By.Id("column-body-Open")));
-        var startButton = openColumn.FindElement(By.CssSelector(".action-btn-primary"));
-        var cardTitleText = _fixture.GetTextSafely(By.Id("column-body-Open"), By.CssSelector(".card-title"));
+        var card = openColumn.FindElements(By.CssSelector(".board-card")).First(card => card.FindElement(By.CssSelector(".card-title")).Text == "995 Bonus: Images");
 
+        var startButton = openColumn.FindElement(By.CssSelector(".action-btn-primary"));
+        var cardTitleText = card.FindElement(By.CssSelector(".card-title")).Text;
         //Act
         _fixture.SafeClick(startButton);
 
